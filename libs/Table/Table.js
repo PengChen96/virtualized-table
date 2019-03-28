03a5f9a946a6deac7bd112298bf5de8b5264d798
cpg <cpg@servyou.com.cn>
2019-03-29 02:12:48 +0800

    table 高度、宽度固定情况

--- libs/Table/Table.js
+++ libs/Table/Table.js
@@ -3,38 +3,242 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.less';
 
-const columns = [{
-  title: '标题列',
-  width: '100px'
-}, {
-  content: '内容列',
-  width: '100px'
-}];
-
 class Table extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      // 列 #
+      columns: [],
+      // 虚拟列
+      virtualColumns: [],
+      // 源数据 #
+      dataSource: [],
+      // 虚拟数据
+      virtualData: [],
+      // 可视区域高度
+      visibleHeight: 400,
+      // 一行的高度
+      rowHeight: 40,
+      // 可渲染的元素个数
+      rowVisibleCount: 10,
+      // 上下偏移渲染个数
+      rowOffsetCount: 5,
+      // 可视区坐标(rowIndex垂直)
+      startRowIndex: 0,
+      endRowIndex: 0,
+      // padding偏移量(垂直)
+      startVerticalOffset: 0,
+      endVerticalOffset: 0,
+
+
+      // 可视区域宽度
+      visibleWidth: 1200,
+      // 预估的每列宽度
+      estimatedColumnWidth: 150,
+      // 可渲染个数（水平）
+      columnVisibleCount: 8,
+      // 左右偏移渲染个数
+      columnOffsetCount: 4,
+      // 可视区坐标(columnIndex水平)
+      startColumnIndex: 0,
+      endColumnIndex: 0,
+      // padding偏移量(水平)
+      startHorizontalOffset: 0,
+      endHorizontalOffset: 0,
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+
+    console.log(props.dataSource !== state.dataSource);
+    if (props.dataSource !== state.dataSource) {
+      let rowVisibleCount = Math.ceil(state.visibleHeight / state.rowHeight);
+      let endRowIndex = state.startRowIndex + rowVisibleCount + state.rowOffsetCount * 2;
+      //
+      let columnVisibleCount = Math.ceil(state.visibleWidth / state.estimatedColumnWidth)
+      let endColumnIndex = state.startColumnIndex + columnVisibleCount + state.columnOffsetCount * 2;
+
+      return {
+        columns: props.columns,
+        virtualColumns: props.columns.slice(state.startColumnIndex, endColumnIndex),
+        startHorizontalOffset: state.startColumnIndex * state.estimatedColumnWidth,
+        endHorizontalOffset: (props.columns.length - endColumnIndex) * state.estimatedColumnWidth,
+        columnVisibleCount,
+        //
+        dataSource: props.dataSource,
+        virtualData: props.dataSource.slice(state.startRowIndex, endRowIndex),
+        startVerticalOffset: state.startRowIndex * state.rowHeight,
+        endVerticalOffset: (props.dataSource.length - endRowIndex) * state.rowHeight,
+        rowVisibleCount
+      };
+    }
+    return null;
+
+  }
+
+  componentDidMount () {
+    this.setState({
+      visibleHeight: this._container.clientHeight,
+      visibleWidth: this._container.clientWidth
+    });
+  }
+
+  // 垂直方向滚动
+  _onVerticalScroll() {
+    const {scrollTop} = this._container;
+    const {
+      dataSource,
+      rowHeight,
+      rowOffsetCount,
+      rowVisibleCount
+    } = this.state;
+    // 获取垂直滚动的条数
+    let scrollTopNum = Math.floor(scrollTop / rowHeight);
+    // 获取要渲染的行开始坐标，最小坐标为0  rowOffsetCount: 行偏移量
+    let startRowIndex = (scrollTopNum - rowOffsetCount) > 0 ? (scrollTopNum - rowOffsetCount) : 0;
+    // 获取要渲染的行结尾坐标，最大坐标为dataSource长度  rowOffsetCount: 行偏移量
+    let endRowIndex = (rowVisibleCount + scrollTopNum + rowOffsetCount) > dataSource.length ? dataSource.length : (rowVisibleCount + scrollTopNum + rowOffsetCount);
+    // 上方未渲染数据的paddingTop值
+    let startVerticalOffset = startRowIndex * rowHeight;
+    // 上方未渲染数据的paddingBottom值
+    let endVerticalOffset = (dataSource.length - endRowIndex) * rowHeight;
+    // 需要渲染显示的行数据
+    let virtualData = dataSource.slice(startRowIndex, endRowIndex);
+    console.log(scrollTopNum, startRowIndex, endRowIndex, startVerticalOffset, endVerticalOffset, virtualData);
+    this.setState({
+      startRowIndex,
+      endRowIndex,
+      startVerticalOffset,
+      endVerticalOffset,
+      virtualData
+    });
+  }
+
+  // 水平方向滚动
+  _onHorizontalScroll() {
+    const {scrollLeft} = this._container;
+    const {
+      columns,
+      estimatedColumnWidth,
+      columnOffsetCount,
+      columnVisibleCount
+    } = this.state;
+    // let totalColumnLength = this.state.columns.length;
+    // 获取水平滚动的条数
+    let scrollLeftNum = Math.floor(scrollLeft / estimatedColumnWidth);
+    // 获取要渲染的列开始坐标
+    let startColumnIndex = (scrollLeftNum - columnOffsetCount) > 0 ? (scrollLeftNum - columnOffsetCount) : 0;
+    // let startColumnIndex = scrollLeftNum;
+    // 获取要渲染的列结尾坐标
+    let endColumnIndex = (columnVisibleCount + scrollLeftNum + columnOffsetCount) > columns.length ? columns.length : (columnVisibleCount + scrollLeftNum + columnOffsetCount);
+    // let endColumnIndex = columnVisibleCount + scrollLeftNum;
+    // 左边未渲染数据的paddingLeft值
+    let startHorizontalOffset = startColumnIndex * estimatedColumnWidth;
+    // 右边未渲染数据的paddingRight值
+    let endHorizontalOffset = (columns.length - endColumnIndex) * estimatedColumnWidth;
+    // 需要渲染显示的列数据
+    let virtualColumns = columns.slice(startColumnIndex, endColumnIndex);
+    console.log(scrollLeftNum, startColumnIndex, endColumnIndex, startHorizontalOffset, endHorizontalOffset, virtualColumns);
+    this.setState({
+      startColumnIndex,
+      endColumnIndex,
+      startHorizontalOffset,
+      endHorizontalOffset,
+      virtualColumns
+    });
+  }
+
+  // 滚动事件
+  _onScrollEvent() {
+    console.log(this);
+    // 垂直方向滚动
+    this._onVerticalScroll();
+    // 水平方向滚动
+    this._onHorizontalScroll();
+
+    // 当前的滚动位置 减去  上一次的滚动位置
+    // 如果为true则代表向下滚动，false代表向上滚动
+    const {scrollPosition} = this.state;
+    let flagToDirection = this._container.scrollTop - scrollPosition > 0;
+    // 记录当前的滚动位置
+    this.setState({
+      scrollPosition: this._container.scrollTop
+    });
+    const height = this._container.scrollHeight - this._container.scrollTop - this._container.clientHeight;
+    // 记录滚动位置距离底部的位置
+    let scrollBottom = this._container.scrollHeight - (this._container.scrollTop + this._container.clientHeight) < 100;
+    // 如果已达到指定位置则触发 （向下滚动）
+    if (flagToDirection && scrollBottom) {
+    }
+    // 向上滚动
+    if (!flagToDirection && this._container.scrollTop < 100) {
+    }
+    console.table({
+      'scrollTop 滚动的高度': this._container.scrollTop,
+      'scrollLeft 滚动的宽度': this._container.scrollLeft,
+      'clientHeight 视窗高度': this._container.clientHeight,
+      'scrollHeight 页面高度': this._container.scrollHeight,
+      'height 距离页面底部的高度': height,
+      '滚动方向': flagToDirection ? '下' : '上'
+    });
+  }
+
+  _cellRender(row, rowIndex, column, columnIndex) {
+    let realRowIndex = rowIndex + this.state.startRowIndex;
+    let realColumnIndex = columnIndex + this.state.startColumnIndex;
+    let value = row[column['key']];
+    return <div style={{minWidth: 150}}>
+      {
+        column.render ? column.render(value) : row[column['key']]
+      }
+      [{realRowIndex}, {realColumnIndex}]
+    </div>;
   }
 
   render() {
 
-    const {list} = this.props;
-    let columnCount = 2;
+    const {
+      virtualColumns,
+      startHorizontalOffset,
+      endHorizontalOffset,
+      visibleWidth,
+      //
+      virtualData,
+      startVerticalOffset,
+      endVerticalOffset,
+      rowHeight
+    } = this.state;
 
     return (
-      <div className="v-table-container">
-        {this.props.title}
-        {
-          list.map((item, index) => {
-            return <React.Fragment key={index}>
-              <div className="v-table-row">
-                <div>{item.title}</div>
-                <div>{item.content}</div>
-              </div>
-            </React.Fragment>;
-          })
-        }
+      <div className="v-table-container"
+        ref={c => this._container = c}
+        onScrollCapture={this._onScrollEvent.bind(this)}
+      >
+        <div style={{paddingTop: startVerticalOffset, paddingBottom: endVerticalOffset}}>
+          {
+            virtualData.map((row, rowIndex) => {
+              return <React.Fragment key={rowIndex}>
+                <div className="v-table-row" style={{
+                  height: rowHeight,
+                  width: visibleWidth,
+                  paddingLeft: startHorizontalOffset,
+                  paddingRight: endHorizontalOffset
+                }}>
+                  {
+                    virtualColumns.map((column, columnIndex) => {
+                      return <React.Fragment key={columnIndex}>
+                        {
+                          this._cellRender(row, rowIndex, column, columnIndex)
+                        }
+                      </React.Fragment>;
+                    })
+                  }
+                </div>
+              </React.Fragment>;
+            })
+          }
+        </div>
       </div>
     );
 
@@ -45,7 +249,8 @@ class Table extends React.Component {
 Table.propTypes = {
   title: PropTypes.string,
   columns: PropTypes.array,
-  list: PropTypes.array
+  list: PropTypes.array,
+  num: PropTypes.number
 };
 
 export default Table;
--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ class App extends React.Component {
       list: []
     };
   }
-  getList(){
+  getList(val = ''){
     let list = [];
     for (let i = 0; i < 10000; i++) {
       list.push({
-        title: '标题'+i,
-        content: '内容'+i
+        title: '标题' + i + val,
+        content: '内容' + i + val
       });
       this.setState({
         list: list
@@ -23,14 +23,32 @@ class App extends React.Component {
     }
   };
   render() {
+    const columns = [];
+    for (let i = 0; i < 5000; i++) {
+      columns.push({
+        key: 'title',
+        title: '标题列',
+        width: '100px',
+        render: (value) => {
+          return <span>{value}+</span>;
+        }
+      });
+      columns.push({
+        key: 'title',
+        title: '内容列',
+        width: '100px'
+      });
+    }
     const {list} = this.state;
     console.log(list);
     return (
       <div className="App">
-        <div onClick={() => this.getList()}>getlist</div>
+        <div onClick={() => this.getList()}>getList</div>
+        <div onClick={() => this.getList('哈哈哈哈哈哈')}>getListVal</div>
         <Table
           title="title"
-          list={list}
+          columns={columns}
+          dataSource={list}
         />
       </div>
     );
