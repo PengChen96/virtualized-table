66a09b3a937c5e5b8cbc223056202c3988b1f04f
pengchen <2601349541@qq.com>
2019-10-31 21:49:48 +0800

    add: VTable2.0 - save

--- libs/VTable2.0/Grid.js
+++ libs/VTable2.0/Grid.js
@@ -14,7 +14,7 @@ const Grid = (props) => {
     // 可视区域高度
     visibleHeight: props.visibleHeight || 400,
     // 一行的高度（预估）
-    estimatedRowHeight: props.estimatedRowHeight || 40,
+    estimatedRowHeight: props.estimatedRowHeight || 10,
     // 可渲染的元素个数
     rowVisibleCount: props.rowVisibleCount || 10,
     // 上下偏移渲染个数
@@ -47,7 +47,7 @@ const Grid = (props) => {
 
   const _onScrollEvent = () => {
 
-    console.log('121');
+    console.log(_scrollContainer.current.scrollTop);
     _onVerticalScroll();
 
   };
@@ -72,8 +72,7 @@ const Grid = (props) => {
     let endVerticalOffset = (dataSource.length - endRowIndex) * estimatedRowHeight;
     // 需要渲染显示的行数据
     let virtualData = dataSource.slice(startRowIndex, endRowIndex);
-    console.log(virtualData);
-    setGrid(Object.assign({
+    setGrid(Object.assign(grid, {
       startRowIndex,
       endRowIndex,
       startVerticalOffset,
@@ -86,7 +85,7 @@ const Grid = (props) => {
 
     let value = row[column['key']];
     let width = column.width || 150;
-    return <div className="v-grid-cell" style={{
+    return <div className="vt-grid-cell" style={{
       width: width,
       minWidth: width
     }}>
@@ -96,32 +95,18 @@ const Grid = (props) => {
   };
 
   return <>
-    <div className="v-grid-container1"
+    <div className="vt-grid-container"
       ref={_scrollContainer}
       onScrollCapture={() => _onScrollEvent()}
       style={{height: stateProps.visibleHeight}}
     >
-      <div className="v-grid-main-container">
+      <div style={{
+        paddingTop: grid.startVerticalOffset,
+        paddingBottom: grid.endVerticalOffset
+      }}>
         {
-          props.dataSource.map((row, rowIndex) => {
-            return <div className="v-grid-row" key={rowIndex}>
-              {
-                props.columns.map((column, columnIndex) => {
-                  return <div key={columnIndex}>
-                    {
-                      _cellRender(row, rowIndex, column, columnIndex)
-                    }
-                  </div>;
-                })
-              }
-            </div>;
-          })
-        }
-      </div>
-      <div className="v-grid-left-columns-container">
-        {
-          props.dataSource.map((row, rowIndex) => {
-            return <div className="v-grid-row" key={rowIndex}>
+          grid.virtualData.map((row, rowIndex) => {
+            return <div className="vt-grid-row" key={rowIndex}>
               {
                 props.columns.map((column, columnIndex) => {
                   return <div key={columnIndex}>
